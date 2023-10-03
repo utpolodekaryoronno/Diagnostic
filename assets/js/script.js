011d18990b874f6de0e4f57fc2f0dbbfdcbf2b81
Utpol Odekary Oronno <104867628+utpolodekaryoronno@users.noreply.github.com>
2023-10-04 02:16:34 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // Mobile Menu Icon
+   $(document).ready(function(){
+      $('#nav-icon').click(function(){
+         $(this).toggleClass('open');
+         $('.header-nav nav').slideToggle();
+      });
+   });
+
+});
